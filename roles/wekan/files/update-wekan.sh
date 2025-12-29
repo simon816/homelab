4b850792fa4e6f706780323024aef8eae8e55bb7
simon816 <commits@simon816.com>
2025-12-29 00:26:07 +0000

    Fix wekan updater script

--- roles/wekan/files/update-wekan.sh
+++ roles/wekan/files/update-wekan.sh
@@ -5,14 +5,15 @@ set -ex
 sudo rm -rf /tmp/wekan-update
 mkdir /tmp/wekan-update
 cd /tmp/wekan-update
-wget https://releases.wekan.team/SHA256SUMS.txt
-wget https://releases.wekan.team/wekan-latest-amd64.zip
-sed -i 's/wekan-.*\.zip$/wekan-latest-amd64.zip/; /\.zip$/! d' 'SHA256SUMS.txt'
-sha256sum -c SHA256SUMS.txt
+
+latest=$(curl -s https://api.github.com/repos/wekan/wekan/tags | python3 -c 'import json, sys; print(json.load(sys.stdin)[0]["name"])')
+filename="wekan-$(echo $latest | sed s/^v//)-amd64.zip"
+
+wget "https://github.com/wekan/wekan/releases/download/$latest/$filename"
 
 mkdir extract
 sudo chown www-data:www-data extract
-sudo -u www-data unzip wekan-latest-amd64.zip -d extract
+sudo -u www-data unzip $filename -d extract
 sudo systemctl stop wekan
 
 sudo rm -rf /var/www/wekan/
@@ -22,7 +23,9 @@ cd /var/www/wekan/programs/server
 
 sudo rm -rf /tmp/wekan-update
 
-sudo rm node_modules/.bin/node-pre-gyp node_modules/.bin/node-gyp node_modules/.bin/detect-libc node_modules/node-gyp/node_modules/.bin/semver node_modules/@npmcli/fs/node_modules/.bin/semver
+sudo rm node_modules/.bin/node-pre-gyp \
+    node_modules/.bin/node-gyp \
+    node_modules/.bin/detect-libc
 sudo -u www-data NPM_CONFIG_CACHE=/tmp/npmcache npm install node-gyp node-pre-gyp fibers
 sudo -u www-data NPM_CONFIG_CACHE=/tmp/npmcache npm audit fix
 sudo -u www-data NPM_CONFIG_CACHE=/tmp/npmcache npm install
